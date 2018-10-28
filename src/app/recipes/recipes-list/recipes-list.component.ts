388cfd3afd4583737689f45407885f726172b1e0
amberchen <ya810720@gmail.com>
2018-10-28 20:00:54 +0800

    add recipe

---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css']
+})
+export class RecipesListComponent implements OnInit {
+
+  recipes:Recipe[] = [
+    new Recipe('Test','Just Test','https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
